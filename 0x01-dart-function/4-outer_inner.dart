2a61648360cb34d37cf1ea0f2b2cb8f3d5bc4e66
linszay <lindseyjlancaster@gmail.com>
2024-02-19 21:52:24 -0600

    outer inner

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,3 +1,9 @@
 void outer(String name, String id) {
-    
+    print("Hello Agent B.${inner(name)} your id is $id");
+}
+
+String inner(String name) {
+    List<String> splitName = name.split(" ");
+    String lastName = splitName[1];
+    return "$lastName";
 }
