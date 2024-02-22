5dfb19d2019b3534692d0e841ce888a18298cb79
linszay <lindseyjlancaster@gmail.com>
2024-02-22 02:58:20 -0600

    fixed issues with main file not recognizing private property

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,24 +1,28 @@
 class Password {
-  String _password = '';
+    String _password = '';
 
-  bool isValid() {
-    if (_password.length < 8 || _password.length > 16) {
-      return false;
+    Password({String password}) {
+        _password = password;
     }
-    if (!_password.contains(RegExp(r'[A-Z]'))) {
-      return false;
-    }
-    if (!_password.contains(RegExp(r'[a-z]'))) {
-      return false;
-    }
-    if (!_password.contains(RegExp(r'[0-9]'))) {
-      return false;
-    }
-    return true;
-  }
 
-  @override
-  String toString() {
-    return 'Your Password is: $_password';
-  }
+    bool isValid() {
+        if (_password.length < 8 || _password.length > 16) {
+            return false;
+        }
+        if (!_password.contains(RegExp(r'[A-Z]'))) {
+            return false;
+        }
+        if (!_password.contains(RegExp(r'[a-z]'))) {
+            return false;
+        }
+        if (!_password.contains(RegExp(r'[0-9]'))) {
+            return false;
+        }
+        return true;
+    }
+
+    @override
+    String toString() {
+        return 'Your Password is: $_password';
+    }
 }
