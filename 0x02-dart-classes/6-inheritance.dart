f326596a0e0cbbed475bdca2a39b0023416d800d
linszay <lindseyjlancaster@gmail.com>
2024-02-26 16:45:33 -0600

    inheritance

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,34 @@
+import '6-password.dart';
+class User {
+    int id = 0;
+    String name = '';
+    int age = 0;
+    double height = 0.0;
+    String? user_password;
+    
+    User({required this.id, required this.name, required this.age, required this.height, required this.user_password});
+
+    Map<String, dynamic> toJson() {
+        return {
+        'id': id,
+        'name': name,
+        'age': age,
+        'height': height,
+        'user_password': user_password,
+        };
+    }
+
+    static User fromJson(Map<dynamic, dynamic> userJson) {
+        return User(
+            id: userJson['id'],
+            name: userJson['name'],
+            age: userJson['age'],
+            height: userJson['height'],
+            user_password: userJson['user_password'],
+        );
+    }
+
+    String toString() {
+        return 'User(id : $id ,name: $name, age: $age, height: $height, $user_password)';
+    }
+}
