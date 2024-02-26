23a581ce136d44f55c66c2ab7cd0d3bb2191072b
linszay <lindseyjlancaster@gmail.com>
2024-02-26 16:19:23 -0600

    json to user class

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,30 @@
+class User {
+    String id = '';
+    String name = '';
+    int age = 0;
+    double height = 0.0;
+    
+    User({required this.id, required this.name, required this.age, required this.height});
+
+    Map<String, dynamic> toJson() {
+        return {
+        'id': id,
+        'name': name,
+        'age': age,
+        'height': height,
+        };
+    }
+
+    static User fromJson(Map<dynamic, dynamic> userJson) {
+        return User(
+            id: userJson['id'],
+            name: userJson['name'],
+            age: userJson['age'],
+            height: userJson['height'],
+        );
+    }
+
+    String toString() {
+        return 'User(id: $id, name: $name, age: $age, height: $height)';
+    }
+}
