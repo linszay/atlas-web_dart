50c0bd4a42fc0909d45e29c0a4a2488bf5874024
linszay <lindseyjlancaster@gmail.com>
2024-02-28 11:34:59 -0600

    get user id

---
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -0,0 +1,7 @@
+import '1-util.dart';
+
+Future<String> getUserId() async {
+  final userData = await fetchUserData();
+  final userMap = json.decode(userData) as Map<String, dynamic>;
+  return userMap['id'] as String;
+}
