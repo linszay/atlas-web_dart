56e0d866c308b39d0fd2044ed17fefaa20fafce5
linszay <lindseyjlancaster@gmail.com>
2024-02-28 13:17:49 -0600

    greet user

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,29 @@
+import '3-util.dart';
+
+Future<String> greetUser() async {
+    try {
+        final userData = await fetchUserData();
+        final username = userData['username'];
+        return 'Hello $username';
+        } catch (error) {
+            return 'error caught: $error';
+        }
+}
+
+Future<String> loginUser() async {
+    try {
+        final isUser = await checkCredentials();
+        print('There is a user: $isUser');
+        if (isUser) {
+        return await greetUser();
+        } else {
+        return 'Wrong credentials';
+        }
+    } catch (error) {
+        return 'error caught: $error';
+    }
+}
+
+void main() async {
+    print(await loginUser());
+}
