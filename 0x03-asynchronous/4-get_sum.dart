b0080aedc99356522e379f23c40b1400aa92fa13
linszay <lindseyjlancaster@gmail.com>
2024-02-28 14:17:32 -0600

    changed handling of double

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -2,18 +2,18 @@ import '4-util.dart';
 import 'dart:convert';
 
 Future<double> calculateTotal() async {
-  try {
-    final userData = await fetchUserData();
-    final userOrders = await fetchUserOrders(json.decode(userData)['id']);
-    
-    double totalPrice = 0;
-    for (var product in json.decode(userOrders)) {
-      final productPrice = await fetchProductPrice(product);
-      totalPrice += json.decode(productPrice);
+    try {
+        final userData = await fetchUserData();
+        final userOrders = await fetchUserOrders(json.decode(userData)['id']);
+        
+        double totalPrice = 0;
+        for (var product in json.decode(userOrders)) {
+            final productPrice = await fetchProductPrice(product);
+            totalPrice += double.parse(productPrice);
+        }
+        
+        return totalPrice;
+    } catch (e) {
+        return -1;
     }
-    
-    return totalPrice;
-  } catch (e) {
-    return -1;
-  }
 }
