94008eb8b6e8aa62e4d728598d0852025f82144a
linszay <lindseyjlancaster@gmail.com>
2024-02-28 11:37:29 -0600

    needed to import dart:convert

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
 import '1-util.dart';
 
 Future<String> getUserId() async {
-  final userData = await fetchUserData();
-  final userMap = json.decode(userData) as Map<String, dynamic>;
-  return userMap['id'] as String;
+    final userData = await fetchUserData();
+    final userMap = json.decode(userData) as Map<String, dynamic>;
+    return userMap['id'] as String;
 }
